799c708eb47dbad6eb1451180e34010cda58ea94
martuman95 <73444049+martuman95@users.noreply.github.com>
2021-09-12 18:53:32 +0200

    added time and date

--- src/Weather.js
+++ src/Weather.js
@@ -1,9 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 import "./Weather.css";
+import axios from "axios";
+import FormatDate from "./FormatDate";
 
 
 export default function Weather(props) {
-   return (
+    const [weatherData, setWeatherData] = useState({ ready: false });
+    const [temperature, setTemperature] = useState(null);
+ function handleResponse(response) {
+    setWeatherData({
+        ready:true,
+         date: new Date(response.data.dt * 1000) ,
+         temperature: Math.round(response.data.main.temp),
+         humidity: response.data.main.humidity,
+         wind: response.data.wind.speed,
+city: response.data.name,
+description: response.data.weather[0].description
+     })
+     
+     
+ }
+ 
+ if (weatherData.ready) {
+ return (
        <div>
 <div className>
             <form className="Weather">
@@ -13,21 +32,29 @@ export default function Weather(props) {
        </ div>
             <div className="title">
        <h2>
-           London
+           {weatherData.city}
        </h2>
          </div>
         <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" />
-  <span className="temperature">17</span> <span className="degrees">°C</span>
+  <span className="temperature">{weatherData.temperature}</span> <span className="degrees">°C</span>
     <ul>
-         <p> Tuesday 9:42 </p> 
-        <li>Partly Cloudy </li>
+         <p> <FormatDate date={weatherData.date}/> </p> 
+        <li className="text-capitolize">{weatherData.description} </li>
         <li>
-            Humidity: 32 %
+            Humidity: {weatherData.humidity} %
         </li>
         <li>
-            Wind Speed: 3 Km/h
+            Wind Speed: {weatherData.wind}  Km/h
         </li>
     </ul>
- </div>
+    </div>
     )
+ } else {
+    let apiKey = "f5ee6fe9739269adb6179e45323cceb3"
+    let city = "London";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+   axios.get(apiUrl).then(handleResponse);
+   return "Loading..."
+   }
+ 
 }
