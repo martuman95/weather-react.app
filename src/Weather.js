d4f47e9a29345aa76a84b554fac580877f632f84
martuman95 <73444049+martuman95@users.noreply.github.com>
2021-09-16 09:22:48 +0200

    fixed problem

--- src/Weather.js
+++ src/Weather.js
@@ -10,41 +10,49 @@ import WeatherTemperature from "./WeatherTemperature";
 export default function Weather(props) {
     const [weatherData, setWeatherData] = useState({ ready: false });
  const[city, setCity] = useState(props.defaultCity);
-    function handleResponse(response) {
+   
+ function handleResponse(response) {
     setWeatherData({
         ready:true,
          date: new Date(response.data.dt * 1000) ,
          temperature: Math.round(response.data.main.temp),
          humidity: response.data.main.humidity,
          wind: response.data.wind.speed,
-city: response.data.name,
-description: response.data.weather[0].description,
-icon:  response.data.weather[0].icon
+         city: response.data.name,
+         description: response.data.weather[0].description,
+         icon:  response.data.weather[0].icon
      })
  }
- 
+ function handleSubmit(event) {
+ event.preventDefault();
+ search();
+ }
+  function handleCityChange(event) {
+setCity(event.target.value);
+ }
+
 function search(){
  let apiKey = "f5ee6fe9739269adb6179e45323cceb3"
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
    axios.get(apiUrl).then(handleResponse);
 }
 
-function handleSubmit(event) {
- event.preventDefault();
- }
-
- function handleCityChange(event) {
-setCity(event.target.value);
-search();
- }
 
  if (weatherData.ready) {
  return (
        <div>
 <div className>
-            <form className="Weather" onSubmit={handleSubmit} >
-        <input type="text" placeholder="Type a city" onChange={handleCityChange}/> 
-    <button> Let's go </button>
+            <form 
+            className="Weather" 
+            onSubmit={handleSubmit} >
+        <input 
+        type="text" 
+        placeholder="Type a city" 
+        onChange={handleCityChange}/> 
+        <input
+                className="btn"
+                type="submit"
+                value="Let's go" />
        </form>
        </ div>
             <div className="title">
